233e895895a99534e5088f0c3b0336510ba81c42
DiggsPapu <alo20172@uvg.edu.gt>
2023-03-25 20:54:56 -0600

    Rule of 120 char

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,24 @@
 module.exports = {
+  meta: {
+    type: 'layout',
+    docs: {
+      description: 'Description of the rule',
+    },
+    fixable: 'code',
+    schema: [
+      {
+        type: 'array',
+        items: [
+          {
+            enum: ['always'],
+          },
+        ],
+      },
+    ],
+    messages: {
+      lineBiggerThan120: 'Line bigger than 120 characters',
+    },
+  },
   env: {
     browser: true,
     es2021: true,
---
+++ src/eslint/maxCharRule.js
@@ -0,0 +1,22 @@
+module.exports = {
+    meta: {
+        type: 'layout',
+        docs: {
+            description: 'The max length of the line code is 120, so this rule will check that out'
+        },
+        fixable: 'code',
+        schema: [
+            {
+                type: 'array',
+                items: [
+                    {
+                        enum: ['always'],
+                    }
+                ]
+            }
+        ],
+        messages: {
+            lineBiggerThan120: 'Line bigger than 120 char.',
+        }
+    },
+}
