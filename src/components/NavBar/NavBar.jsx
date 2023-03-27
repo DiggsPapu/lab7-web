94c387ac935e6999d23b2c9d86d14a009090a61e
DiggsPapu <alo20172@uvg.edu.gt>
2023-03-27 11:25:33 -0600

    navbar links

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -3,7 +3,7 @@ import './NavBar.css'
 import NavMenu from "./NavMenu"
 
 function NavBar() {
-  const val = [1,2,3,4,5,6,7]
+  
   return (
     <nav className="navigation">
       <button className="hamburger" type="button">
@@ -13,6 +13,28 @@ function NavBar() {
       props={["DUCKDUCKGO SEARCH","Private Search","https://duckduckgo.com/","Themes",
       "https://duckduckgo.com/settings#theme","All Settings","https://duckduckgo.com/settings",
       "!Bang Search Shortcuts","https://duckduckgo.com/bangs"]}
+      />
+      <NavMenu 
+      props={["DOWNLOADS","iOS","",
+      "Android",
+"https://play.google.com/store/apps/details?id=com.duckduckgo.mobile.android&referrer=utm_source%3Dstatic-sidemenu",
+      "Mac","https://duckduckgo.com/mac",
+      "Browse Extension","https://duckduckgo.com/app"]}
+      />
+      <NavMenu 
+      props={["WHY PRIVACY","Privacy Blog","https://spreadprivacy.com/","Privacy Newsletter",
+      "https://duckduckgo.com/newsletter","Help Spread DuckDuckGo!","https://duckduckgo.com/spread",
+      "",""]}
+      />
+      <NavMenu 
+      props={["LEARN MORE","About Us","https://duckduckgo.com/about","Privacy Policy",
+      "https://duckduckgo.com/privacy","Careers","https://duckduckgo.com/hiring",
+      "Press Kit","https://duckduckgo.com/press"]}
+      />
+      <NavMenu 
+      props={["KEEP IN TOUCH","Twitter","https://twitter.com/","Reddit",
+      "https://reddit.com","Help","https://help.duckduckgo.com/",
+      "Advertise on Seearch","https://help.duckduckgo.com/company/advertise-on-duckduckgo-search"]}
       />
     </nav>
   )
--- src/components/NavBar/NavMenu.jsx
+++ src/components/NavBar/NavMenu.jsx
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 import React from "react"
 import './NavBar.css'
 import PropTypes, { string } from 'prop-types'
