94c90e3837fe0157bcfaf5b6d98cab605b6f48dd
DiggsPapu <alo20172@uvg.edu.gt>
2023-03-28 16:28:58 -0600

    Cards section

---
+++ src/components/Card/CardSection.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import './Card.css'
+import Card from './Card'
+
+function CardSection(){
+    return <div className='wrapper'>
+    <h1>Privacy Protection For Any Device</h1>
+        <div className="card-section">
+    <Card props={["https://duckduckgo.com/assets/add-to-browser/cppm/laptop.svg","Privacy Browser Extension",
+"Browse as usual, and we'll take care of the rest. Get bundled private search,", 
+"tracker blocking, and site encryption, all in one download, for major browsers."
+    ]}/>
+    <Card props={["https://duckduckgo.com/assets/home/landing/icons/search.svg","Privacy Search Engine",
+    "Search privately with our app or extension, add private web search to your favorite browser,",
+    "or search directly at duckduckgo.com."]}/>
+    <Card props={["https://duckduckgo.com/assets/add-to-browser/cppm/mobile.svg","Privacy Browser App",
+    "Our private browser for mobile comes equipped with our search engine, tracker blocker,",
+    " encryption enforcer, and more. Available on iOS & Android."]}/>
+    </div>
+    </div>
+}
+export default CardSection
--- src/components/FirstPart/SectionOne.jsx
+++ src/components/FirstPart/SectionOne.jsx
@@ -3,7 +3,7 @@ import SearchBar from "../SearchBar/SearchBar"
 import DuckDuckGoLogo from '../../assets/DuckDuckGo_logo.svg'
 // import DisplayInfo from "../DisplayInfo/DisplayInfo"
 import './SectionOne.css'
-import Card from '../Card/Card'
+import CardSection from "../Card/CardSection"
 
 function SectionOne(){
     return <div>
@@ -16,18 +16,7 @@ function SectionOne(){
         <button type="button" >Add DuckDuckGo to Edge</button>
         <p>Trusted by tens of millions worldwide!</p>
         </div>
-        <div className="card-section">
-        <Card props={["https://duckduckgo.com/assets/add-to-browser/cppm/laptop.svg","Privacy Browser Extension",
-"Browse as usual, and we'll take care of the rest. Get bundled private search,", 
-"tracker blocking, and site encryption, all in one download, for major browsers."
-        ]}/>
-        <Card props={["https://duckduckgo.com/assets/home/landing/icons/search.svg","Privacy Search Engine",
-        "Search privately with our app or extension, add private web search to your favorite browser,",
-        "or search directly at duckduckgo.com."]}/>
-        <Card props={["https://duckduckgo.com/assets/add-to-browser/cppm/mobile.svg","Privacy Browser App",
-        "Our private browser for mobile comes equipped with our search engine, tracker blocker,",
-        " encryption enforcer, and more. Available on iOS & Android."]}/>
-        </div>
+        <CardSection/>
     </div>
 }
 export default SectionOne
